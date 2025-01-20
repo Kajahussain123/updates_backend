f672639852eb1bb511b47ccb2c8c1efc6a190b76
hafishali <hafishali4@gmail.com>
2025-01-20 16:04:16 +0530

    change rrot url

--- server.js
+++ server.js
@@ -24,15 +24,15 @@ app.use(express.json());
 //  Admin Routes 
 // app.use('/users', UserRoutes);
 app.use('/api/admin',adminDetails)
-app.use('/api//category',categoryDetails)
-app.use('/api//product-details', productDetails);
-app.use('/api//testimonials', testimonails);
+app.use('/api/category',categoryDetails)
+app.use('/api/product-details', productDetails);
+app.use('/api/testimonials', testimonails);
 
 
 // // User Routes
-app.use('/api//mobile',mobileDetails)
-app.use('/api//CCTV-AC', cctvDetails);
-app.use('/api//accessory',accessoryDetails)
+app.use('/api/mobile',mobileDetails)
+app.use('/api/CCTV-AC', cctvDetails);
+app.use('/api/accessory',accessoryDetails)
 
 // using upload folder that contain images
 app.use('/uploads',express.static('./uploads'))
